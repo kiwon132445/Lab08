35e7c1cc68e1ea3699109738434a825197903bbd
Philip Bolinao <pdbolinao@gmail.com>
2019-04-01 15:21:01 -0700

    style changes

--- static/JS/client.js
+++ static/JS/client.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
 var menuButton = $('#menuButton'),
     menu = $('#menuResp'),
     menuAnimSlideIn = {"top": "128px", "easing": "swing"},
-    menuAnimSlideOut = {"top": "0px", "easing": "swing"},
+    menuAnimSlideOut = {"top": "-128px", "easing": "swing"},
     showMenu = false;
 
 $(menuButton).click(function (event) {
         if (!showMenu) {
             showMenu = true;
-            $(menu).animate(menuAnimSlideIn, 300);
+            $(menu).animate(menuAnimSlideIn, 100);
         } else {
             showMenu = false;
-            $(menu).animate(menuAnimSlideOut, 500);
+            $(menu).animate(menuAnimSlideOut, 100);
         }
     });
 });
